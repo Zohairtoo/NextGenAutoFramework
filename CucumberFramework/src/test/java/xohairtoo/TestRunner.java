8f9cf8cda6f7adc421fac324faec2c73505842b8
zohairtoo <zohairtoo@gmail.com>
2026-05-12 17:44:56 -0400

    relocated ActiveProle annotation to SringIntegrationTest.java

--- CucumberFramework/src/test/java/xohairtoo/TestRunner.java
+++ CucumberFramework/src/test/java/xohairtoo/TestRunner.java
@@ -8,7 +8,6 @@ import org.springframework.test.context.ActiveProfiles;
         glue = {"xohairtoo"},
         plugin = {"pretty", "html:target/cucumber-reports/"+ "cucumber-pretty.html", "json:target/cucumber-reports/cucumber.json"}
 )
-@ActiveProfiles("rest")
 public class TestRunner extends AbstractTestNGCucumberTests {
     // This class is empty, it just acts as the runner
 
